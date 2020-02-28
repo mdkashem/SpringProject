21937c5ff3131d0865efc99fa9f0732797e13fb2
Md Abul Kashem <56566230+mdkashem@users.noreply.github.com>
2020-02-28 14:49:26 -0800

    add code

---
+++ src/com/md/TestOB.java
@@ -0,0 +1,13 @@
+package com.md;
+
+public class TestOB {
+
+	private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+}
